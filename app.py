0d351b1cc52d2909cf6790cf7bbf3ceed3e847d7
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-06 15:40:09 -0400

    added requirements

--- app.py
+++ app.py
@@ -1,15 +1,4 @@
-from flask import Flask
-from PyQt5.QtCore import *
-from PyQt5.QtWebChannel import *
-from PyQt5.QtWidgets import QApplication
-from threading import Timer
-import sys
+from flask import *
 
-Zeno = Flask(__name__)
-
-@Zeno.route("/")
-def test():
-    return "Exit 0"
-
-def ui(location):
-    qt_app = QApplication(sys.argv)
+app = Flask(__name__)
+ui = WebUI
