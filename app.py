806973c10c4fc3b7afa11305059a7ac178078b8b
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-06 15:36:18 -0400

    use test flask route

--- app.py
+++ app.py
@@ -4,3 +4,12 @@ from PyQt5.QtWebChannel import *
 from PyQt5.QtWidgets import QApplication
 from threading import Timer
 import sys
+
+Zeno = Flask(__name__)
+
+@Zeno.route("/")
+def test():
+    return "Exit 0"
+
+def ui(location):
+    qt_app = QApplication(sys.argv)
